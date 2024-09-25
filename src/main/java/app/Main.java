b83911ae15605630a074342527e5c5c5df94c84e
Shiihon <144337411+Shiihon@users.noreply.github.com>
2024-09-25 10:34:31 +0200

    added routes in main and poemcontroller

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,18 @@
 package app;
 
+import app.controllers.PoemController;
+import io.javalin.Javalin;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Javalin app = Javalin.create(config -> {
+            config.router.contextPath = "/api/poems"; // all base pat for all routes
+            config.bundledPlugins.enableRouteOverview("/routes"); // enables route overview at /routes
+        });
+
+        PoemController.addRoutes(app);
+
+        app.start(7000);
     }
 }
---
+++ src/main/java/app/controllers/PoemController.java
@@ -0,0 +1,50 @@
+package app.controllers;
+
+import app.DAOs.PoemDAO;
+import app.DTOs.PoemDTO;
+import app.config.HibernateConfig;
+import app.entities.Poem;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import jakarta.persistence.EntityManagerFactory;
+
+import java.util.Set;
+
+public class PoemController {
+    private static EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("poems");
+    private static PoemDAO poemDAO = new PoemDAO(emf);
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static void addRoutes(Javalin app) {
+        app.get("/", ctx -> getAllPoems(ctx));
+    }
+
+    private static void getAllPoems(Context ctx) {
+        Set<PoemDTO> poems = poemDAO.getAll();
+        ctx.json(poems, PoemDTO.class);
+    }
+
+    private static void getPoemById(Context ctx) {
+        String id = ctx.pathParam("id");
+        ctx.status(200);
+        ctx.json(poemDAO.getById(Long.parseLong(id)), PoemDTO.class);
+    }
+
+    private static void createPoem(Context ctx) throws JsonProcessingException {
+        String jSonString = ctx.body();
+        PoemDTO poemDTO = objectMapper.readValue(jSonString, PoemDTO.class);
+        ctx.status(201);
+        poemDAO.create(poemDTO);
+    }
+
+    private static void updatePoem(Context ctx) {
+
+    }
+
+    private static void deletePoem(Context ctx) {
+
+    }
+}
