8e61bab6b9794be21e403f181f6319baed48ad97
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-09-25 10:12:37 +0200

    Add PoemDAO getById and getAll

--- src/main/java/app/DAOs/PoemDAO.java
+++ src/main/java/app/DAOs/PoemDAO.java
@@ -4,6 +4,11 @@ import app.DTOs.PoemDTO;
 import app.entities.Poem;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.TypedQuery;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class PoemDAO {
     private EntityManagerFactory emf;
@@ -21,5 +26,24 @@ public class PoemDAO {
             em.getTransaction().commit();
         }
         return new PoemDTO(poem);
+    }
+
+    public PoemDTO getById(Long id) {
+        try (EntityManager em = emf.createEntityManager()) {
+            Poem poem = em.find(Poem.class, id);
+
+            if (poem == null) {
+                throw new EntityNotFoundException(String.format("Poem with id %d could not be found", id));
+            }
+
+            return new PoemDTO(poem);
+        }
+    }
+
+    public Set<PoemDTO> getAll() {
+        try (EntityManager em = emf.createEntityManager()) {
+            TypedQuery<Poem> query = em.createQuery("SELECT p FROM Poem p", Poem.class);
+            return query.getResultStream().map(PoemDTO::new).collect(Collectors.toSet());
+        }
     }
 }
