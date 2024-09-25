010ba362b2d48dff8db51cfabfeb78bf4fe9ac23
Shiihon <144337411+Shiihon@users.noreply.github.com>
2024-09-25 11:55:00 +0200

    added JsonIgnore to getAsEntity

--- src/main/java/app/DTOs/PoemDTO.java
+++ src/main/java/app/DTOs/PoemDTO.java
@@ -2,6 +2,7 @@ package app.DTOs;
 
 import app.entities.Poem;
 import app.enums.PoemType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,6 +23,7 @@ public class PoemDTO {
         this.poem = poem.getPoem();
     }
 
+    @JsonIgnore
     public Poem getAsEntity() {
         return Poem.builder()
                 .author(author)
